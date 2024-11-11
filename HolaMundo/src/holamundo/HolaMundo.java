92fc28b977a1b2371ad6e7f69ec3250e583ce0d1
Tomeu01 <tft00@iesemilidarder.com>
2024-11-11 19:30:30 +0100

    Add files via upload

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+/**
+ *
+ * @author tomeu
+ */
+public class HolaMundo {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
